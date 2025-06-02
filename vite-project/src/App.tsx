a7a6bf9817b6bff456debc321998ea8582f1bfb1
MarioAng38 <137503248+MarioAng38@users.noreply.github.com>
2025-06-02 21:51:19 +0300

    added alert and buttons

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,20 +1,44 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 
 function App() {
   let items = ["Targoviste", "Bucuresti", "Comisani", "Constanta"];
 
+  const [alertVisible, setAlertVisibility] = useState(false);
+
   const handleSelectItem = (item: string) => {
     console.log(item);
   };
 
+  const [color, setColor] = useState<
+    "primary" | "secondary" | "danger" | "success"
+  >("primary");
+
+  const toggleColor = () => {
+    setColor((prev) => (prev === "primary" ? "success" : "primary"));
+    setAlertVisibility(true);
+  };
+
   return (
-    <div>
-      <ListGroup
-        items={items}
-        heading="Orase"
-        onSelectItem={handleSelectItem}
-      />
-    </div>
+    <>
+      <div className="d-flex justify-content-center align-items-center my-5">
+        <ListGroup
+          items={items}
+          heading="Orase"
+          onSelectItem={handleSelectItem}
+        />
+      </div>
+      <div className="d-flex justify-content-center align-items-center my-5">
+        {alertVisible && <Alert>Butonul a fost apasat!</Alert>}
+      </div>
+      <div className="d-flex justify-content-center align-items-center my-5">
+        <Button onClick={toggleColor} color={color}>
+          My button
+        </Button>
+      </div>
+    </>
   );
 }
 
--- vite-project/src/Message.tsx
+++
@@ -1,7 +0,0 @@
-function Message() {
-  const name = "Lume";
-  if (name) return <h1>Hello {name}</h1>;
-  return <h1>Hello World</h1>;
-}
-
-export default Message;
